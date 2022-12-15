bdac6d907505044b95b33d2c34de4ffe091f4294
mosherif21 <mo.sherif1992@gmail.com>
2022-12-15 20:27:07 +0200

    initial commit

--- lib/src/constants/common_functions.dart
+++ lib/src/constants/common_functions.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-import '../../mqtt/mqtt_browser_class.dart';
+import '../../mqtt/mqtt_server_class.dart';
 import '../features/home_page/screens/home_page_screen.dart';
 
 double getScreenHeight(BuildContext context) {
@@ -16,11 +16,9 @@ double getScreenWidth(BuildContext context) {
 var mqttClient;
 
 Future<void> initializeMqttClient() async {
-  mqttClient = MQTTClientBrowserWrapper();
+  // mqttClient = MQTTClientBrowserWrapper();
+  mqttClient = MQTTClientServerWrapper();
   await mqttClient.prepareMqttClient();
-
-  // mqttClient = MQTTClientServerWrapper();
-  // await mqttClient.prepareMqttClient();
 }
 
 void getToHomePage() async {
